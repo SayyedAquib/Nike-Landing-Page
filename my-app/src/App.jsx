09827f3575979a1ef0bf063bb66eb9c11e4cd8f1
Sayyed Aquib <72184419+SayyedAquib@users.noreply.github.com>
2023-08-23 16:49:59 +0000

    First commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Naviagtion from './components/Navigation';
+import Body from './components/Body';
+
+function App() {
+
+  return (
+    <>
+      <Naviagtion />
+      <Body />
+    </>
+  );
+
+}
+
+export default App
---
+++ my-app/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Naviagtion = () => {
+    return (
+        <nav>
+            <div className='logo'>
+                <img src="images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li>MENU</li>
+                <li>LOCATIOIN</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+    );
+}
+
+export default Naviagtion;
